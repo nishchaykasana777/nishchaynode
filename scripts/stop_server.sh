041afa2f4783da187a1827096d2aca50ba8a7b84
root <root@ip-172-31-1-89.ap-south-1.compute.internal>
2023-03-09 08:09:55 +0000

    hi

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-sudo systemctl stop nishchaynode.service
+# Find the process ID (PID) of the Node.js server process
+PID=$(ps aux | grep '[n]ode server.js' | awk '{print $2}')
+
+# If the process ID exists, kill the process
+if [[ -n $PID ]]; then
+  echo "Stopping Node.js server with PID $PID"
+  kill $PID
+else
+  echo "No Node.js server process found"
+fi
+
 
